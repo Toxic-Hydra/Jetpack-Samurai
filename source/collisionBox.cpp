6e60766dcb2e8f38414124da2b947ea5e48058ea
Charles Advincula <cadvincu@ucsc.edu>
2020-02-23 20:57:41 -0800

    Still need to add physics to collision boxes

--- source/collisionBox.cpp
+++ source/collisionBox.cpp
@@ -7,5 +7,16 @@ CollisionBox::CollisionBox(int x, int y, int width, int height)
 
 bool CollisionBox::collidesWith(CollisionBox &otherBox)
 {
+    if (box.left < otherBox.right &&
+        box.right > otherBox.left &&
+        box.top < otherBox.bottom &&
+        box.bottom > otherBox.top)
+    {
+        // Move both boxes depending on relative forces (velocity x mass)
+        // Code here
+        //
+        /////////////////////////////////////////////////////////////////
+        return true;
+    }
     return false;
 }
